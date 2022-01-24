3b4505014d89dffd45674ceeed96dc71157dc4f2
Qusarun <94105803+Qusarun@users.noreply.github.com>
2022-01-24 20:50:11 +0300

    Update primes.c

--- primes.c
+++ primes.c
@@ -16,7 +16,7 @@ bool is_prime(int num, int* primes, int primeCount) {
         return true;
 }
 
-int main() {
+int main(void) {
         int primes[10000];
         int primeCount = 0;
         for (int i = 0; i < 10000; i++) {
